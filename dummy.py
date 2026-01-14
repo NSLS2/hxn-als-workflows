09fa10524c205061bdc7f18015ed3f3be89c1eb5
Jun Aishima <jaishima@bnl.gov>
2026-01-14 16:29:06 -0500

    use a flow instead

--- dummy.py
+++ dummy.py
@@ -3,7 +3,7 @@ from prefect.blocks.system import Secret
 from tiled.client import from_profile
 
 
-@task
+@flow
 def handle_uri(uri="https://tiled.nsls2.bnl.gov"):
     logger = get_run_logger()
     logger.info(f"URI: {uri}")
