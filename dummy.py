ca550a12cbcca5becb5a29e1dda1377a8a67ecf0
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 12:16:36 -0500

    modify the api key in dummy.py

--- dummy.py
+++ dummy.py
@@ -4,7 +4,7 @@ from prefect.blocks.system import Secret
 
 @flow
 def handle_uri(uri="https://tiled.nsls2.bnl.gov"):
-    tiled_api_key = Secret.load("tiled-hxn-api-key")
+    tiled_api_key = Secret.load("tiled-synaps-api-key")
     api_key_value = tiled_api_key.get()
     
     client = from_uri(uri, api_key=api_key_value)
