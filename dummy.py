913d01adc539147e70485acab08a41eb039b990f
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 10:20:01 -0500

    try tiled import

--- dummy.py
+++ dummy.py
@@ -1,7 +1,10 @@
 from prefect import flow, get_run_logger
+import scipy
+from tiled.client import from_uri
 
 
 @flow
 def handle_uri(uri="https://tiled.nsls2.bnl.gov"):
+    client = from_uri(uri)
     logger = get_run_logger()
-    logger.info(f"URI: {uri}")
+    logger.info(list(client))
