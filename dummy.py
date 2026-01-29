dddf80734e42af633f28229557a6a4cffadf88bc
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 14:54:54 -0500

    started echo flow

--- dummy.py
+++ dummy.py
@@ -3,10 +3,12 @@ from tiled.client import from_uri
 from prefect.blocks.system import Secret
 
 @flow
-def handle_uri(uri="https://tiled.nsls2.bnl.gov"):
+def handle_uri(uri_in="https://tiled.nsls2.bnl.gov", uri_out="https://tiled.nsls2.bnl.gov"):
     tiled_api_key = Secret.load("tiled-synaps-api-key")
     api_key_value = tiled_api_key.get()
     
-    client = from_uri(uri, api_key=api_key_value)
+    reader = from_uri(uri_in, api_key=api_key_value)
+    writer = from_uri(uri_out, api_key=api_key_value)
+
     logger = get_run_logger()
-    logger.info(list(client))
+    logger.info(list(reader))
