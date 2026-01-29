f9fe8c526e3104496b2cd748466cf9e7459a29d8
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 10:32:53 -0500

    try adding tiled api key

--- dummy.py
+++ dummy.py
@@ -4,7 +4,7 @@ from prefect.blocks.system import Secret
 
 @flow
 def handle_uri(uri="https://tiled.nsls2.bnl.gov"):
-    tiled_api_key = Secret.load("my-api-key-name")
+    tiled_api_key = Secret.load("TILED_API_KEY")
     api_key_value = tiled_api_key.get()
     
     client = from_uri(uri, api_key=api_key_value)
